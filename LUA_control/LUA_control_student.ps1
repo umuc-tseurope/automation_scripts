9b558eb7ba9ec580fb374000da52e579e6afa4fd
James Nicholson <james.nicholson@umuc.edu>
2016-05-17 11:50:25 +0200

    Setup user config/creation for student lab/support use.

--- LUA_control/LUA_control_student.ps1
+++ LUA_control/LUA_control_student.ps1
@@ -1,5 +1,5 @@
 ﻿########################################
-# User account script stub
+# User account script stub for student lab/support PC's
 # 
 # This is the basis of testing the automation of local user accounts in
 # Windows 7 deployments. This script will be run against newly loaded hardware
@@ -10,3 +10,19 @@
 
 # NOTE: each of these scripts are designed for a specific target
 # Target: support/lab PC
+
+# Remove the unnedded local accounts
+Remove-LocalUser -Name ntctsa -Computername localhost
+Remove-LocalUser -Name XXXX -Computername localhost
+Remove-LocalUser -Name umucfr -Computername localhost
+Remove-LocalUser -Name  -Computername localhost
+
+# Create needed accounts if not already on the system
+New-LocalUser -Name student -Password maryland -Computername localhost -Description (Student user account)
+New-LocalUser -Name maintenance -Password Fixit4me -Computername localhost -Description (maintenance account)
+New-LocalUser -Name tsupport -Password Arizona1941 -Computername localhost -Description (admin account)
+
+# Ensure that the student, maintenenace, and tsupport accounts are enabled
+Enable-LocalUser -Name student -Computername localhost
+Enable-LocalUser -Name tsupport -Computername localhost
+Enable-LocalUser -Name maintenance -Computername localhost

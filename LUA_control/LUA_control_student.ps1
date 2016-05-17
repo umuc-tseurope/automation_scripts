fe2c685455c7c392195e8b7fb487b22845a3a71d
James Nicholson <james.nicholson@umuc.edu>
2016-05-17 10:26:58 +0200

    Added subdirectory and stub for local account management scripts.

---
+++ LUA_control/LUA_control_student.ps1
@@ -0,0 +1,12 @@
+﻿########################################
+# User account script stub
+# 
+# This is the basis of testing the automation of local user accounts in
+# Windows 7 deployments. This script will be run against newly loaded hardware
+# and eliminate the need to manually create/delete user accounts.
+#
+########################################
+
+
+# NOTE: each of these scripts are designed for a specific target
+# Target: support/lab PC

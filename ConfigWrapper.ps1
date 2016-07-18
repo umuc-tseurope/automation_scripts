92667f053dc35ddb17cf00f7b8651187f52dec1c
James Nicholson <james.nicholson@umuc.edu>
2016-07-18 14:43:10 +0200

    Created a wrapper script that runs the working configuration scripts sequencially.

---
+++ ConfigWrapper.ps1
@@ -0,0 +1,14 @@
+﻿#############################################
+# ConfigWrapper.ps1
+#
+# .SYNOPSIS a wrapper script for post image configuration
+#
+#
+#############################################
+
+# This runs the commands in order sequentially
+.\Software_activations\WindowsActivate.ps1;
+.\Software_activations\OfficeActivate.ps1;
+.\Registry_control\reg_win_update_fix.ps1;
+.\OS_configuration\Local_comp_name.ps1;
+.\OS_configuration\Local_desc_property_no.ps1

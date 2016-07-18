0cfb92e16ce00a6ebbd5a9aac3e6c3b664f99607
James Nicholson <james.nicholson@umuc.edu>
2016-07-18 13:21:35 +0200

    Updated Windows activation script and added a script for setting execution policy.

---
+++ OS_configuration/SetExePolicy.ps1
@@ -0,0 +1,9 @@
+﻿###################################################
+# SetExePolicy.ps1
+#
+# .SYNOPSIS sets the execution policy of the system
+#
+#
+###################################################
+
+Set-ExecutionPolicy remotesigned -Force
--- Software_activations/WindowsActivate.ps1
+++ Software_activations/WindowsActivate.ps1
@@ -12,7 +12,7 @@ $computer = gc env:computername
 # Read in the file with the config data
 $configData = Import-Clixml "C:\Windows\System32\tsdata.xml"
 
-$key = $configData.Windows7
+$key = $configData.productkeys.Windows7
 
 $service = Get-WMIObject -query “select * from SoftwareLicensingService” -computername $computer
 
